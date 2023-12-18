1de0bb00d5d3ca7005c580284c49ba0caa39525a
DELLOON <delloondev@gmail.com>
2023-12-18 13:03:09 +0800

    Дописал основной функционал программы

--- UP/Properties/AssemblyInfo.cs
+++ UP/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("UP")]
+[assembly: AssemblyTitle("Satisfactory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UP")]
+[assembly: AssemblyProduct("Satisfactory")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
