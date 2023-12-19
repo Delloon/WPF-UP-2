20dc06beb68c064f2668d1eb0b8c387cd96093cf
DELLOON <delloondev@gmail.com>
2023-12-19 16:10:55 +0800

    оптимизировал немного код

--- UP/EditMaterials.xaml.cs
+++ UP/EditMaterials.xaml.cs
@@ -47,26 +47,30 @@ namespace UP
         {
 
             StringBuilder errors = new StringBuilder();
+
             if (string.IsNullOrEmpty(_currentMaterial.Наименование))
                 errors.AppendLine("Укажите название материала");
+
             if (string.IsNullOrEmpty(_currentMaterial.ТипМатериала))
                 errors.AppendLine("Укажите тип материала");
+
             if (_currentMaterial.Цена < 0)
                 errors.AppendLine("Цена не может быть отрицательной");
+
             if (_currentMaterial.КолНаСкладе < 0)
                 errors.AppendLine("Количество материала на складе не может быть отрицательным");
+
             if (_currentMaterial.МинКолНаСкладе < 0)
                 errors.AppendLine("Минимальное количество материала на складе не может быть отрицательным");
+
             if (_currentMaterial.КолУпаковки < 0)
-                errors.AppendLine("Количество материала в одной упаковки не может быть отрицательным");
+                errors.AppendLine("Количество материала в одной упаковке не может быть отрицательным");
+
             if (string.IsNullOrEmpty(_currentMaterial.ЕдиницаИзм))
                 errors.AppendLine("Укажите единицу измерения");
 
-            if (_currentMaterial.IDМатериала == 0 || imgChange == true)
-            {
+            if (_currentMaterial.IDМатериала == 0 || imgChange)
                 _currentMaterial.ИзображениеМатериала = im;
-            }
-
 
             if (errors.Length > 0)
             {
@@ -74,22 +78,23 @@ namespace UP
                 return;
             }
 
-            if (_currentMaterial.IDМатериала == 0)
-            {
-                BussEntities.GetContext().Material.Add(_currentMaterial);
-            }
             try
             {
-                BussEntities.GetContext().SaveChanges();
+                using (var context = BussEntities.GetContext())
+                {
+                    if (_currentMaterial.IDМатериала == 0)
+                        context.Material.Add(_currentMaterial);
+
+                    context.SaveChanges();
+                }
+
                 MessageBox.Show("Информация сохранена!");
                 FrameManager.MainFrame.GoBack();
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message.ToString());
+                MessageBox.Show(ex.Message);
             }
-
-
 
         }
 
